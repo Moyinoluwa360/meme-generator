6c0732e6dba0813cedb530287f72544df7bf2186
Moyinoluwa360 <ogundanajoseph17@gmail.com>
2024-10-01 12:40:42 +0100

    finished the main component styling

---
+++ src/components/Input.jsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+
+export default function InputArea(props){
+    return (
+        <InputSec>
+            <input type="text" 
+            placeholder="Top Text"
+            value={props.topText}
+            onChange={(e)=> props.setTopText(e.target.value)}/>
+            <input type="text" 
+            value={props.bottomText}
+            onChange={(e)=> props.setBottomText(e.target.value)}/>
+            <button>Generate Meme</button>
+        </InputSec>
+    )
+}
+
+const InputSec = styled.div`
+    height: 30%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    align-items: center;
+    margin-top: 10px;
+    input{
+        width: 90%;
+        height: 20%;
+        border-radius: 10px;
+        border-style: none;
+        padding-left: 20px;
+        background-color: #85888b;
+        color: aliceblue;
+        font-size: 20px;
+        @media (max-width: 760px){
+            width: 90%;
+        }
+    }
+    button{
+        width: 50%;
+        height: 20%;
+        border-radius: 10px;
+        border-style: none;
+        background-color: #0400ff;
+        font-size: 20px;
+        font-weight: bolder;
+        @media (max-width: 760px){
+            width: 50%;
+        }
+    }
+`
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,22 @@
 import styled from "styled-components";
+import InputArea from "./Input";
+import { useState } from "react";
+
 
 export default function Main(){
+    const [topText, setTopText] = useState("")
+    const [bottomText, setBottomText] = useState("")
     return (
         <MainSection>
             <InnerSection>
-
+                <TopText>{topText}</TopText>
+                <BottomText>{bottomText}</BottomText>
+                <img src="/assets/rock.png" alt="" width={"100%"} height={"60%"}/>
+                <InputArea
+                 bottomText = {bottomText}
+                 topText = {topText}
+                 setBottomText = {setBottomText}
+                 setTopText = {setTopText}></InputArea>
             </InnerSection>
         </MainSection>
     )
@@ -19,10 +31,39 @@ const MainSection = styled.div`
     justify-content: center;
 `
 const InnerSection = styled.div`
-    background-color: #707070;
-    height: 95%;
-    width: 60%;
+    height: 97%;
+    width: 40%;
+    font-size: 35px;
+    font-weight: 600;
     @media (max-width:760px){
         width: 95%;
+        font-size: 30px;
+    }
+    img{
+        margin: 0%;
+    }
+`
+const TopText = styled.div`
+    position: absolute;
+    width: 35%;
+    top: 95px;
+    text-align: center;
+    word-wrap: break-word;
+    left: 32.5%;
+    @media (max-width:760px){
+        max-width: 85%;
+        left: 7%;
+    }
+`
+const BottomText = styled.div`
+    position: absolute;
+    top:52% ;
+    left: 32.5%;
+    width: 35%;
+    text-align: center;
+    word-wrap: break-word;
+    @media (max-width:760px){
+        max-width: 85%;
+        left: 7%;
     }
 `
